9f0ca3ec4ba1b67a30dc3b2cccaeedc286ea0537
mattchristenson <matt@rcc.team>
2023-11-06 10:01:05 -0600

    changes to demo1 to show different versions and filter.

--- demos/demo1.js
+++ demos/demo1.js
@@ -3,13 +3,29 @@ let numbers = [7, 9, 64, 60, 12, 13, 65, 62];
 
 
 let firstNumberOver60;
+let firstNumberOver60Index;
 
 for( let i = 0; i < numbers.length ; i++){
     if(numbers[i] > 60){
         firstNumberOver60 = numbers[i];
+        firstNumberOver60Index = i;
         break;
     }
 }
+
+// for( let number of numbers){
+//     if(number > 60){
+//         firstNumberOver60 = number;
+//         break;
+//     }
+// }
+
+// for (let number of numbers){
+//     if(isOver60(number)){
+//         firstNumberOver60 = number;
+//         break;
+//     }
+// }
 
 console.log(`The first number over 60 is ${firstNumberOver60}`);
 
@@ -20,6 +36,22 @@ function isOver60(someValue) {
 }
 
 let firstNumberOver60UsingFind = numbers.find(isOver60);
+let firstNumberOver60UsingFindIndex = numbers.findIndex(isOver60);
 
-console.log(`The first number over 60 is ${firstNumberOver60UsingFind}`);
+console.log(`The first number over 60 is ${firstNumberOver60UsingFind} at index ${firstNumberOver60UsingFindIndex}`);
 
+
+let allOver60 = [];
+
+for( let number of numbers){
+    if(isOver60(number)){
+        allOver60.push(number);
+    }
+}
+
+console.log(numbers);
+console.log(allOver60);
+
+let allOver60WithFilter = numbers.filter(isOver60);
+
+console.log(allOver60WithFilter);
